78311b59bfcbcc8e3cc4a927fe793cc6f16f3115
djakkalap <thedoge2706@gmail.com>
2019-10-22 12:36:09 +0200

    Added disable command

---
+++ ZombiesBreakDoors/ZombiesBreakDoors/ZBDDisableCommand.cs
@@ -0,0 +1,28 @@
+﻿using Smod2;
+using Smod2.Commands;
+using Smod2.Attributes;
+using Smod2.Config;
+using Smod2.EventHandlers;
+
+namespace ZombiesBreakDoors {
+    class ZBDDisableCommand : ICommandHandler {
+        private readonly ZBDPlugin plugin;
+
+        public ZBDDisableCommand(ZBDPlugin plugin) => this.plugin = plugin;
+
+        public string GetCommandDescription() {
+            return "Disables the ZombiesBreakDoors plugin.";
+        }
+
+        public string GetUsage() {
+            return "ZBDDISABLE";
+        }
+
+        public string[] OnCall(ICommandSender sender, string[] args) {
+            plugin.Info("Disabling " + plugin.Details.name + "...");
+            plugin.PluginManager.DisablePlugin(plugin);
+
+            return new string[] {plugin.Details.name + " disabled."};
+        }
+    }
+}
--- ZombiesBreakDoors/ZombiesBreakDoors/ZBDPlugin.cs
+++ ZombiesBreakDoors/ZombiesBreakDoors/ZBDPlugin.cs
@@ -5,12 +5,12 @@ using Smod2.EventHandlers;
 
 namespace ZombiesBreakDoors {
     [PluginDetails(
-        author = "Argon",
+        author = "djakkalap",
         name = "Zombies Break Doors",
         description = "This plugin allows zombies to gather near a door and break it.",
-        id = "argon.zbdplugin",
+        id = "djakkalap.zbdplugin",
         configPrefix = "zbd",
-        version = "0.2",
+        version = "0.3",
         SmodMajor = 3,
         SmodMinor = 5,
         SmodRevision = 0
@@ -34,6 +34,9 @@ namespace ZombiesBreakDoors {
             this.AddConfig(new ConfigSetting("zbd_zombies_range", 2.5f, true, "This number determines how far the zombies need to be in range for them to be considered 'near the door'."));
             this.AddConfig(new ConfigSetting("zbd_breakopendoors", false, true, "This boolean determines whether or not to break open doors."));
             this.AddConfig(new ConfigSetting("zbd_delay", 2.0f, true, "This number determines how many seconds it takes for a door to break."));
+            this.AddConfig(new ConfigSetting("zbd_disable", false, true, "This boolean decides whether to load the plugin."));
+
+            this.AddCommand("zbddisable", new ZBDDisableCommand(this));
 
             this.AddEventHandler(typeof(IEventHandlerDoorAccess), new BreakDoorHandler(this));
         }
